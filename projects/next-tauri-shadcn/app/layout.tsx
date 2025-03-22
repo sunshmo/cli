ef22597351c7b832b3f133359b02e98cd8c0b03b
sunshmo <sunsilently@outlook.com>
2025-03-22 22:07:09 +0800

    chore: 修改配置

--- projects/next-tauri-shadcn/app/layout.tsx
+++ projects/next-tauri-shadcn/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import React from 'react';
 import './globals.scss';
 import { ThemeProvider } from 'next-themes';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Next App',
@@ -28,7 +17,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         suppressHydrationWarning
-        className={`${geistSans.variable} ${geistMono.variable} antialiased root-layout`}
+        className={`antialiased root-layout`}
       >
         <ThemeProvider>
           {children}
